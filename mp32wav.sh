4ffa77a0ba3f10a167a72be6b3cd34dbb3af0386
qxod <107460267+qxod@users.noreply.github.com>
2021-07-25 17:07:48 +0000

    release 1.1

--- mp32wav.sh
+++ mp32wav.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-./gstfs -d -osrc=$1,src_ext=mp3,dst_ext=wav,pipeline="filesrc name=\"_source\" ! de
+./gstfs -d -osrc=$1,src_ext=mp3,dst_ext=wav,pipeline="filesrc name=\"_source\" ! decodebin ! 
