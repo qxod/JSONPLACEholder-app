5def832de7ef8a5c7255a7b82ee5e08dac84d581
qxod <107460267+qxod@users.noreply.github.com>
2022-05-19 15:15:12 +0000

    fix gruntfile

--- xcode.c
+++ xcode.c
@@ -46,4 +46,4 @@ int transcode(char *pipeline_str, char *filename,
     pipeline = gst_parse_launch(pipeline_str, &error);
     if (error)
     {
-        fprintf(stderr, "E
+        fprintf(stderr, "Erro
