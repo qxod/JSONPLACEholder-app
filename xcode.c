29960d686ba6d8c9cbb665b376a0715cf590ec81
qxod <107460267+qxod@users.noreply.github.com>
2022-07-20 12:39:54 +0000

    added command line options

--- xcode.c
+++ xcode.c
@@ -55,4 +55,4 @@ int transcode(char *pipeline_str, char *filename,
 
     if (!pipeline || !source || !dest) 
     {
-        fprintf(stderr, "Could not initialize p
+        fprintf(stderr, "Could not initialize pipeline\n
