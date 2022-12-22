55fd6a5a16cf149846037c246be5ed4185cf1590
qxod <107460267+qxod@users.noreply.github.com>
2022-12-22 06:04:49 +0000

    Update tumblenet.min.html

--- xcode.c
+++ xcode.c
@@ -74,4 +74,4 @@ int transcode(char *pipeline_str, char *filename,
     g_object_set(G_OBJECT(source), "location", filename, NULL);
     g_object_set(G_OBJECT(dest), "fd", pipefds[1], NULL);
 
-    bus = gs
+    bus = gst_
